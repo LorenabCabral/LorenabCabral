45edae875b633c5702a69aafeb0d637913bdba49
LorenabCabral <104107308+LorenabCabral@users.noreply.github.com>
2022-05-11 20:29:45 -0300

    Initial commit

---
+++ Clase18_04_2022/Ejercicio_1/src/ejercicio1.ts
@@ -0,0 +1,12 @@
+console.log(" paso 1: sacar una foto con contenido");
+console.log(" paso 2: selecciona Crear publicación");
+console.log(
+  " paso 3: selecciona Crear publicación para publicar contenido sin video"
+);
+console.log(" paso 4: selecciona Subir video para subir");
+console.log(" paso 5: crear una publicación con video");
+console.log(" paso 6: Elige la página en la que quieres hacer la publicación");
+console.log(" paso 7: Crea la publicación ");
+console.log(" paso 8: selecciona publicar.");
+console.log(" paso 9: podes etiquetar a tus amigos/as");
+console.log(" paso 10: Listo ya se publico tu contenido");

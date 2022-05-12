8745b377b9117f2c42b60eecf4e313d3cc43fb1a
LorenabCabral <104107308+LorenabCabral@users.noreply.github.com>
2022-05-12 00:03:36 -0300

    ejercicio_2

---
+++ Clase22_04_2022/Ejercicio_2/src/ejercicio2.ts
@@ -0,0 +1,37 @@
+/*
+MODIFICAR EL EJEMPLO DE SECUENCIA:
+QUE CADA MENSAJE SE ALMACENE EN UNA VARIABLE 
+A MOSTRAR POR CONSOLA Y QUE EL FUNCIONAMIENTO 
+DEL SCRIPT SEA EL MISMO*/
+let paso1: string = "este es mi primer paso";
+let paso2: string = "este es mi segundo paso";
+let paso3: string = "este es mi tercer paso";
+let paso4: string = "este es mi cuarto paso";
+let paso5: string = "este es mi quinto paso";
+let paso6: string = "este es mi sexto paso";
+let paso7: string = "este es mi septimo paso";
+let paso8: string = "este es mi octavo paso";
+let paso9: string = "este es mi noveno paso";
+let paso10: string = "este es mi decimo paso";
+
+console.log(paso1);
+console.log(paso2);
+console.log(paso3);
+console.log(paso4);
+console.log(paso5);
+console.log(paso6);
+console.log(paso7);
+console.log(paso8);
+console.log(paso9);
+console.log(paso10);
+
+/* MODIFICAR EL EJEMPLO DE BASE POR ALTURA:
+ALMACENAR LA BASE Y LA ALTURA EN VARIABLES Y EL
+RESULTADO Y QUE EL FUNCIONAMIENTO DEL SCRPT SEA
+EL MISMO*/
+
+let base: number = 5;
+let altura: number = 10;
+let resultado: number = base * altura;
+
+console.log(resultado);

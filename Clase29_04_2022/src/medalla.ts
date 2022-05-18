717eb9b9064b48655feea94a00423f4553f15a42
LorenabCabral <104107308+LorenabCabral@users.noreply.github.com>
2022-05-18 12:43:26 -0300

    medalla

---
+++ Clase29_04_2022/src/medalla.ts
@@ -0,0 +1,21 @@
+let dato = document.getElementById ("dato");
+let rotulo1 = document.getElementById ("rotulo1");
+let btnEnv = document.getElementById ("enviar");
+
+rotulo1.innerHTML = "Ingrese la posicion de llegada del competidor:";
+
+btnEnv.addEventListener ("click" , () => {
+  let posicionLlegada: number = Number (dato.value);
+  if (posicionLlegada == 1) {
+  console.log ("Entregar Medalla de Oro");
+} else {
+  if (posicionLlegada == 2) {
+  console.log ("Entregar Medalla de Plata");
+} else {
+  if (posicionLlegada == 3) {
+  console.log ("Entregar Medalla de Bronce");
+} else {
+  if (posicionLlegada =>4 ) {
+  console.log ("Entregar Mencion de participacio");
+
+});

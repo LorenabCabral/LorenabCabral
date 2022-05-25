adfd66403c67a87cba5971d1a532be0052a171d9
LorenabCabral <104107308+LorenabCabral@users.noreply.github.com>
2022-05-25 19:12:26 -0300

    suma entre numeros

---
+++ Clase05_05_2022/src/sumanumeros.ts
@@ -0,0 +1,26 @@
+let rotulo1 = document.getElementById("rotulo1");
+let rotulo2 = document.getElementById("rotulo2");
+
+rotulo1.innerHTML = "Ingrese Primer Numero";
+rotulo2.innerHTML = "Ingrese Segundo Numero";
+
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let btnEnv = document.getElementById("enviar");
+
+btnEnv.addEventListener("click",() => {
+ let primerNumero: number = Number (dato1.value);
+ let segundoNumero: number = Number (dato2.value);
+ 
+ let actual,suma:number;
+ actual = primerNumero;
+ suma = 0;
+ while ( actual <= segundoNumero){
+   suma+=actual++; 
+ }
+console.log("El resultado de la suma es:",suma);
+
+/*escriba un programa que pida al usuariodos numeros
+enteros, y luego retorne la suma de todos los numeros que estan entre ellos
+por ejemplo, si los numeros son 2 y 7 debe entregar como resultado 2+3+4+5+6+7=27
+*/
